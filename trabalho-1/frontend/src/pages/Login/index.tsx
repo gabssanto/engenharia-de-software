575844359eeaa841f840fc2a52df851fd50c92e9
gsanto <espiritosanto.gabriel@gmail.com>
2021-03-05 10:35:00 -0300

    ueun

--- trabalho-1/frontend/src/pages/Login/index.tsx
+++ trabalho-1/frontend/src/pages/Login/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 
 const Login: React.FC = () => {
+  const [count, setCount] = useState(0);
   return (
       <Container>
           <h1>Hello World</h1>
+          {count}
+          <button style={{ padding: '20px'}} onClick={() => setCount(count+1)}>Add</button>
       </Container>
   );
 }
--- trabalho-1/frontend/src/pages/Login/styles.tsx
+++ trabalho-1/frontend/src/pages/Login/styles.tsx
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   color: #fff;
+  font-size: 48px;
 `;
