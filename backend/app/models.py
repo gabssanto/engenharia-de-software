d6fa16e7916c1e822fd2ca6e2ae0979a21f28d73
Gustavo S Lacerda <61591132+Drewthyson@users.noreply.github.com>
2021-06-01 22:45:06 -0300

    adicionado tabela N pra N no banco de dados

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,11 @@
 from datetime import datetime
-from app import db
+from backend.app import db
+
+userInProject = db.Table('userInProject',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('projeto_id', db.Integer, db.ForeignKey('projeto.id'), primary_key=True),
+    db.Column('permissao_id', db.Integer, db.ForeignKey('permissao.id'), primary_key=True)
+)
 
 
 class User(db.Model):
@@ -7,15 +13,28 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
+    inPermission = db.relationship('permissao', secondary=userInProject, lazy='subquery',
+                                   backref=db.backref('permUser', lazy='dynamic'))
+    inProject = db.relationship('projeto', secondary=userInProject, lazy='subquery',
+                                backref=db.backref('projectUser', lazy='dynamic'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 
-class Kanban(db.Model):
+class permissao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    items = db.Column(db.String)
-    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    moderator = db.Column(db.Boolean, index=True)
+    add_user = db.Column(db.String(64))
+    Remove_user = db.Column(db.String(64))
 
     def __repr__(self):
-        return '<Kanban {}>'.format(self.body)
+        return '<perm {}>'.format(self.moderator)
+    
+    
+class projeto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chats = db.relationship('chat', backref='projeto', lazy=True)
+
+    def __repr__(self):
+        return '<projeto {}>'.format(self.id)
