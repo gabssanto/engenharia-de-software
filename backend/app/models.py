0a86769851ea53d380e006a20bcefda4a4cca862
gsanto <espiritosanto.gabriel@gmail.com>
2021-06-02 13:37:50 -0300

    fixed models

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,13 +7,13 @@ class UserInProject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-    permission_id = db.Column(db.Integer, db.ForeignKey('permission.id'))
 
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True)
+    moderator = db.Column(db.Boolean, index=True, default=False)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     userInProject = db.relationship('UserInProject', backref=db.backref('userInProject', uselist=False))
@@ -22,51 +22,32 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
-class Permission(db.Model):
-    __tablename__ = 'permission'
-    id = db.Column(db.Integer, primary_key=True)
-    moderator = db.Column(db.Boolean, index=True)
-    add_user = db.Column(db.String(64))
-    Remove_user = db.Column(db.String(64))
-
-    def __repr__(self):
-        return '<perm {}>'.format(self.moderator)
-
-
 class Project(db.Model):
     __tablename__ = 'project'
     id = db.Column(db.Integer, primary_key=True)
-    chats = db.relationship('Chat', backref=db.backref('chat', uselist=False), lazy=True)
+    history = db.Column(db.String(), index=True)
 
     def __repr__(self):
-        return '<Project {}>'.format(self.id)
-
-
-class Chat(db.Model):
-    __tablename__ = 'chat'
-    id = db.Column(db.Integer, primary_key=True)
-    history = db.Column(db.String(), index=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'),
-                           nullable=False)
+        return '<Project {}>'.format(self.chats)
 
 
 class Kanban(db.Model):
     __tablename__ = 'kanban'
     id = db.Column(db.Integer, primary_key=True)
-    kanban_items = db.relationship('KanbanItem', backref=db.backref('kanban', uselist=False), lazy=True)
+    kanban_items = db.relationship('KanbanItem', backref=db.backref('kanbanItem', uselist=False), lazy=True)
 
     def __repr__(self):
-        return '<Kanban {}>'.format(self.body)
+        return '<Kanban {}>'.format(self.kanban_items)
 
 
 class KanbanItem(db.Model):
     __tablename__ = 'kanbanItem'
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(45), nullable=False)
+    name = db.Column(db.String(45), nullable=False)
     description = db.Column(db.Text, index=True)
     position = db.Column(db.String(10))
     kanban_id = db.Column(db.Integer, db.ForeignKey('kanban.id'),
                           nullable=False)
 
     def __repr__(self):
-        return '<Kanban {}>'.format(self.body)
+        return '<Kanban {}>'.format(self.id)
